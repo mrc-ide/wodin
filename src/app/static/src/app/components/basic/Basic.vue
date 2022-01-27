33960d076d749b13552e89eed85389f39944f507
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-01-27 15:48:17 +0000

    Add scss file

--- src/app/static/src/app/components/basic/Basic.vue
+++ src/app/static/src/app/components/basic/Basic.vue
@@ -27,6 +27,5 @@
     }
 </script>
 <style lang="scss">
-    @import "../../../../node_modules/bootstrap/scss/bootstrap.scss";
-    @import "../../../../node_modules/bootstrap-vue/src/index.scss";
+    @import "../../../scss/style.scss";
 </style>
