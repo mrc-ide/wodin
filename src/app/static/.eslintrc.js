049b473ef540f3d52847c3e0ded697592fd1db81
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-02-04 18:15:56 +0000

    Add front end linting

---
+++ src/app/static/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        "plugin:vue/vue3-essential",
+        "@vue/airbnb",
+        "@vue/typescript/recommended"
+    ],
+    parserOptions: {
+        ecmaVersion: 2020
+    },
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "comma-dangle": ["error", "never"],
+        indent: ["error", 4],
+        quotes: ["error", "double", { avoidEscape: true }],
+        "arrow-body-style": "off",
+        "import/prefer-default-export": "off",
+        "@typescript-eslint/no-non-null-assertion": "off"
+    },
+    overrides: [{
+        files: [
+            "**/__tests__/*.{j,t}s?(x)",
+            "**/tests/unit/**/*.test.{j,t}s?(x)"
+        ],
+        env: {
+            jest: true
+        },
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off"
+        }
+    }]
+};
