696d4077aac68f815b2059d4a938a70b6d580d1d
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-02-25 09:34:04 +0000

    Use runtimeCompiler option rather than using esm file directly

--- app/static/src/app/basic.ts
+++ app/static/src/app/basic.ts
@@ -1,4 +1,4 @@
-import { createApp } from "vue/dist/vue.esm-bundler";
+import { createApp } from "vue";
 import Vuex from "vuex";
 import { BasicState, storeOptions } from "./store/basic/basic";
 import BasicApp from "./components/basic/BasicApp.vue";
@@ -6,6 +6,5 @@ import BasicApp from "./components/basic/BasicApp.vue";
 export const store = new Vuex.Store<BasicState>(storeOptions);
 
 const app = createApp({components: {BasicApp}});
-//app.component("basic-app", BasicApp);
 app.use(store);
 app.mount("#app");
