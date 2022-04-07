51e983ba452659cbae9ad1b455d971871f8b42a4
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-04-07 14:47:57 +0100

    Add e2e test

---
+++ app/static/tests/e2e/basic.etest.ts
@@ -0,0 +1,27 @@
+import { expect, test, Page } from "@playwright/test";
+
+test.describe("Basic app tests", () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto("/apps/day1");
+    });
+
+    test("renders plot", async ({ page }) => {
+        const plotSelector = ".js-plotly-plot";
+
+        // Test lines are plotted for each of 3 traces
+        const linesSelector = `${plotSelector} .scatterlayer .trace .lines path`;
+        expect((await page.locator(`:nth-match(${linesSelector}, 1)`).getAttribute("d")).startsWith("M0")).toBe(true);
+        expect((await page.locator(`:nth-match(${linesSelector}, 2)`).getAttribute("d")).startsWith("M0")).toBe(true);
+        expect((await page.locator(`:nth-match(${linesSelector}, 3)`).getAttribute("d")).startsWith("M0")).toBe(true);
+
+
+        // Test traces appear on legend
+        const legendTextSelector = `${plotSelector} .legendtext`;
+        expect(await page.innerHTML(`:nth-match(${legendTextSelector}, 1)`)).toBe("y1");
+        expect(await page.innerHTML(`:nth-match(${legendTextSelector}, 2)`)).toBe("y2");
+        expect(await page.innerHTML(`:nth-match(${legendTextSelector}, 3)`)).toBe("y3");
+
+        // Test modebar menu is present
+        expect(await page.isVisible(`${plotSelector} .modebar`)).toBe(true);
+    });
+});
