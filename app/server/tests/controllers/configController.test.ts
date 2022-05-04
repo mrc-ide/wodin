abdf9e63fefe5d03898d284ab3b8212a9e07ede4
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-05-04 18:30:43 +0100

    updated existing controller tests

--- app/server/tests/controllers/configController.test.ts
+++ app/server/tests/controllers/configController.test.ts
@@ -1,13 +1,22 @@
 import { ConfigController } from "../../src/controllers/configController";
 import * as jsonResponse from "../../src/jsonResponse";
 import { ErrorCode } from "../../src/jsonResponse";
+import {ConfigReader} from "../../src/configReader";
 
 describe("configController", () => {
-    const req = {
-        params: {
-            appName: "TestApp"
-        }
-    } as any;
+    const getRequest = (configReader: ConfigReader) => {
+        return {
+            params: {
+                appName: "TestApp"
+            },
+            app: {
+                locals: {
+                    configReader,
+                    appsPath: "app"
+                }
+            }
+        } as any;
+    };
 
     const res = {} as any;
 
@@ -24,26 +33,12 @@ describe("configController", () => {
         jest.resetAllMocks();
     });
 
-    it("registerRoutes adds routes", () => {
-        const mockGet = jest.fn();
-        const app = {
-            get: mockGet
-        } as any;
-
-        const sut = new ConfigController({} as any, "app");
-        sut.registerRoutes(app);
-
-        expect(mockGet.mock.calls.length).toBe(1);
-        expect(mockGet.mock.calls[0][0]).toBe("/config/:appName");
-        expect(mockGet.mock.calls[0][1]).toBe(sut.getConfig);
-    });
-
     it("getConfig reads config file", () => {
         const mockReadConfigFile = jest.fn().mockReturnValue(basicConfig);
         const mockConfigReader = { readConfigFile: mockReadConfigFile } as any;
+        const req = getRequest(mockConfigReader);
 
-        const sut = new ConfigController(mockConfigReader, "app");
-        sut.getConfig(req, res);
+        ConfigController.getConfig(req, res);
 
         expect(mockReadConfigFile.mock.calls.length).toBe(1);
         expect(mockReadConfigFile.mock.calls[0][0]).toBe("app");
@@ -55,8 +50,9 @@ describe("configController", () => {
 
     it("getConfig returns expected error response when app config file is not found", () => {
         const mockConfigReader = { readConfigFile: jest.fn().mockReturnValue(null) } as any;
-        const sut = new ConfigController(mockConfigReader, "app");
-        sut.getConfig(req, res);
+        const req = getRequest(mockConfigReader);
+
+        ConfigController.getConfig(req, res);
 
         expect(spyJsonResponseError.mock.calls.length).toBe(1);
         expect(spyJsonResponseError.mock.calls[0][0]).toBe(404);
