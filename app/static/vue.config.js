11d8889334e79ccafe172d7d0f95f2371c914b5b
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-05-12 15:49:47 +0100

    fix client npm vulnerabilities

--- app/static/vue.config.js
+++ app/static/vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
     lintOnSave: false,
     outputDir: "./dist",
-    runtimeCompiler: true
+    runtimeCompiler: true,
+    configureWebpack: {
+        resolve: {
+            fallback: {
+                stream: require.resolve("stream-browserify"),
+                assert: require.resolve("assert/")
+            }
+        }
+    }
 };
