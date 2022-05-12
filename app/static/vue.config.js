6473f4889ef9aa9c0853d090262d7196b5273b66
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-05-12 16:43:07 +0100

    roll back breaking changes

--- app/static/vue.config.js
+++ app/static/vue.config.js
@@ -1,13 +1,5 @@
 module.exports = {
     lintOnSave: false,
     outputDir: "./dist",
-    runtimeCompiler: true,
-    configureWebpack: {
-        resolve: {
-            fallback: {
-                stream: require.resolve("stream-browserify"),
-                assert: require.resolve("assert/")
-            }
-        }
-    }
+    runtimeCompiler: true
 };
