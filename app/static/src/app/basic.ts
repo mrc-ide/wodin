e1a17055a6548ae950b25baa3e675be1adfcf023
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-06-20 12:09:12 +0100

    Added nav bar

--- app/static/src/app/basic.ts
+++ app/static/src/app/basic.ts
@@ -2,10 +2,11 @@ import { createApp } from "vue";
 import Vuex from "vuex";
 import { storeOptions } from "./store/basic/basic";
 import BasicApp from "./components/basic/BasicApp.vue";
+import AppHeader from "./components/AppHeader.vue";
 import { BasicState } from "./store/basic/state";
 
 export const store = new Vuex.Store<BasicState>(storeOptions);
 
-const app = createApp({ components: { BasicApp } });
+const app = createApp({ components: { BasicApp, AppHeader } });
 app.use(store);
 app.mount("#app");
---
+++ app/static/src/app/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="navbar navbar-dark bg-brand mb-2 px-2">
+    <a class="navbar-brand pl-2" href="/">Course Title</a>
+    <span class="navbar-nav mr-auto">{{ appTitle }}</span>
+    <span class="navbar-text">WODIN v0.0.1</span>
+  </nav>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "AppHeader",
+    props: {
+        appTitle: String
+    }
+});
+</script>
