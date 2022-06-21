e2396ceb9727536e986164fb2b3a01d1544de530
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-06-21 16:58:05 +0100

    lint

--- app/server/src/controllers/appsController.ts
+++ app/server/src/controllers/appsController.ts
@@ -3,13 +3,17 @@ import { AppLocals } from "../types";
 
 export class AppsController {
     static getApp = (req: Request, res: Response) => {
-        const { configReader, appsPath, wodinConfig, wodinVersion } = req.app.locals as AppLocals;
+        const {
+            configReader, appsPath, wodinConfig, wodinVersion
+        } = req.app.locals as AppLocals;
         const { appName } = req.params;
         const config = configReader.readConfigFile(appsPath, `${appName}.config.json`) as any;
         if (config) {
             const view = `${config.appType}-app`;
             // TODO: validate config against schema for app type
-            const viewOptions = { appName, appTitle: config.title, courseTitle: wodinConfig.courseTitle, wodinVersion };
+            const viewOptions = {
+                appName, appTitle: config.title, courseTitle: wodinConfig.courseTitle, wodinVersion
+            };
             res.render(view, viewOptions);
         } else {
             res.status(404).render("app-not-found", { appName });
