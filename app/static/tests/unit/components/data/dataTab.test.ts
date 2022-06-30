97b09e9aaae903f140f5756e7259b5fcec6143a1
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-06-30 14:38:02 +0100

    Added ErrorInfo tests

--- app/static/tests/unit/components/data/dataTab.test.ts
+++ app/static/tests/unit/components/data/dataTab.test.ts
@@ -2,8 +2,12 @@ import { shallowMount } from "@vue/test-utils";
 import DataTab from "../../../../src/app/components/data/DataTab.vue";
 
 describe("Data Tab", () => {
+    const getWrapper = () => {};
+
     it("renders as expected", () => {
         const wrapper = shallowMount(DataTab);
-        expect(wrapper.text()).toBe("Coming soon: Data upload");
+        expect(wrapper.find("h3").text()).toBe("Upload data");
+        expect(wrapper.find("input").attributes("type")).toBe("file");
+        expect(wrapper.find("input").attributes("accept")).toBe(".csv,.txt");
     });
 });
