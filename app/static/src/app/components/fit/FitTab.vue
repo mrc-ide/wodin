039a39ef1f3e81dfb150649d68a2ac750813dc54
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-07-14 15:56:24 +0100

    Add icons to status message

--- app/static/src/app/components/fit/FitTab.vue
+++ app/static/src/app/components/fit/FitTab.vue
@@ -4,7 +4,11 @@
       <button class="btn btn-primary" id="fit-btn" :disabled="!canFitModel" @click="fitModel">Fit model</button>
       <action-required-message :message="actionRequiredMessage"></action-required-message>
       <run-model-plot :fade-plot="!!updateMsg" :model-fit="true"></run-model-plot>
-      <div>Iterations: {{iterations}}  Sum of squares: {{sumOfSquares}}</div>
+      <div v-if="iterations">
+        <vue-feather v-if="converged" class="inline-icon text-success" type="check"></vue-feather>
+        <loading-spinner v-if="fitting" size="xs"></loading-spinner>
+        Iterations: {{iterations}}  Sum of squares: {{sumOfSquares}}
+      </div>
     </div>
   </div>
 </template>
@@ -12,17 +16,21 @@
 <script lang="ts">
 import { computed } from "vue";
 import { useStore } from "vuex";
+import VueFeather from "vue-feather";
 import RunModelPlot from "../run/RunModelPlot.vue";
 import ActionRequiredMessage from "../ActionRequiredMessage.vue";
 import { ModelFitAction } from "../../store/modelFit/actions";
 import {ModelFitGetter} from "../../store/modelFit/getters";
 import userMessages from "../../userMessages";
+import LoadingSpinner from "../LoadingSpinner.vue";
 
 export default {
     name: "FitTab",
     components: {
+      LoadingSpinner,
       RunModelPlot,
-      ActionRequiredMessage
+      ActionRequiredMessage,
+      VueFeather
     },
     setup() {
         const store = useStore();
@@ -31,6 +39,8 @@ export default {
         const canFitModel = computed(() => store.getters[`${namespace}/${ModelFitGetter.canRunFit}`]);
         const fitModel = () => store.dispatch(`${namespace}/${ModelFitAction.FitModel}`);
         const iterations = computed(() => store.state.modelFit.iterations);
+        const converged = computed(() => store.state.modelFit.converged);
+        const fitting = computed(() => store.state.modelFit.fitting);
         const sumOfSquares = computed(() => store.state.modelFit.sumOfSquares);
         const actionRequiredMessage = computed(() => canFitModel.value ? "" : userMessages.modelFit.cannotFit);
 
@@ -38,6 +48,8 @@ export default {
             canFitModel,
             fitModel,
             iterations,
+            converged,
+            fitting,
             sumOfSquares,
             actionRequiredMessage
         };
