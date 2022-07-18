d9ea1307d40d7159d2eac3ccb43779823c96514e
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-07-18 17:52:16 +0100

    Cancel model fit

--- app/static/src/app/components/fit/FitTab.vue
+++ app/static/src/app/components/fit/FitTab.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="fit-tab">
     <div>
-      <button class="btn btn-primary" id="fit-btn" :disabled="!canFitModel" @click="fitModel">Fit model</button>
+      <button class="btn btn-primary me-2" id="fit-btn" :disabled="!canFitModel" @click="fitModel">Fit model</button>
+      <button class="btn btn-primary" id="cancel-fit-btn" :disabled="!fitting" @click="cancelFit">Cancel fit</button>
       <action-required-message :message="actionRequiredMessage"></action-required-message>
       <run-model-plot :fade-plot="!!actionRequiredMessage" :model-fit="true">
         <div v-if="iterations">
           <vue-feather v-if="converged" class="inline-icon text-success" type="check" size="40px"></vue-feather>
+          <vue-feather v-if="cancelled" class="inline-icon text-secondary" type="alert-circle" size="40px"></vue-feather>
           <loading-spinner v-if="fitting" class="inline-icon" size="xs"></loading-spinner>
-          <span>Iterations: {{iterations}}</span>
-          <span class="mx-3">Sum of squares: {{sumOfSquares}}</span>
+          <span class="ms-2">Iterations: {{iterations}}</span>
+          <span class="ms-2">Sum of squares: {{sumOfSquares}}</span>
+          <div v-if="cancelled" class="small text-danger">{{cancelledMsg}}</div>
         </div>
       </run-model-plot>
     </div>
@@ -25,6 +28,7 @@ import { ModelFitAction } from "../../store/modelFit/actions";
 import { ModelFitGetter } from "../../store/modelFit/getters";
 import userMessages from "../../userMessages";
 import LoadingSpinner from "../LoadingSpinner.vue";
+import {ModelFitMutation} from "../../store/modelFit/mutations";
 
 export default {
     name: "FitTab",
@@ -40,18 +44,25 @@ export default {
 
         const canFitModel = computed(() => store.getters[`${namespace}/${ModelFitGetter.canRunFit}`]);
         const fitModel = () => store.dispatch(`${namespace}/${ModelFitAction.FitModel}`);
+        const cancelFit = () => store.commit(`${namespace}/${ModelFitMutation.SetFitting}`, false);
+
         const iterations = computed(() => store.state.modelFit.iterations);
         const converged = computed(() => store.state.modelFit.converged);
         const fitting = computed(() => store.state.modelFit.fitting);
+        const cancelled = computed(() => iterations.value && !fitting.value && !converged.value);
         const sumOfSquares = computed(() => store.state.modelFit.sumOfSquares);
         const actionRequiredMessage = computed(() => (canFitModel.value ? "" : userMessages.modelFit.cannotFit));
+        const cancelledMsg = computed(() => cancelled.value ? userMessages.modelFit.cancelled : "");
 
         return {
             canFitModel,
             fitModel,
+            cancelFit,
             iterations,
             converged,
             fitting,
+            cancelled,
+            cancelledMsg,
             sumOfSquares,
             actionRequiredMessage
         };
