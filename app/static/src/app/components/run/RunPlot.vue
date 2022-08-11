e3e135c8735a06921e5839a9b90866e4e061600e
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-08-11 14:45:59 +0100

    Use WodinPlot in RunPlot

--- app/static/src/app/components/run/RunPlot.vue
+++ app/static/src/app/components/run/RunPlot.vue
@@ -1,150 +1,54 @@
 <template>
-    <div class="run-plot-container" :style="plotStyle">
-      <div class="run-plot" ref="plot">
-      </div>
-      <div v-if="!hasPlotData" class="plot-placeholder">
-        {{ placeholderMessage }}
-      </div>
-      <slot></slot>
-    </div>
+  <wodin-plot
+      :fade-plot="fadePlot"
+      :placeholder-message="placeholderMessage"
+      :end-time="endTime"
+      :plot-data="allPlotData"
+      :solutions="solution ? [solution] : []">
+    <slot></slot>
+  </wodin-plot>
 </template>
 
 <script lang="ts">
-import {
-    computed, defineComponent, ref, watch, onMounted, onUnmounted
-} from "vue";
+import {computed, defineComponent} from "vue";
 import { useStore } from "vuex";
-import { EventEmitter } from "events";
-import {
-    newPlot, react, PlotRelayoutEvent, Plots
-} from "plotly.js";
 import userMessages from "../../userMessages";
 import { odinToPlotly, WodinPlotData } from "../../plot";
+import WodinPlot from "../WodinPlot.vue";
 
 export default defineComponent({
     name: "RunPlot",
     props: {
         fadePlot: Boolean
     },
-    setup(props) {
+    components: {
+        WodinPlot
+    },
+    setup() {
         const store = useStore();
 
         const placeholderMessage = computed(() => (userMessages.run.notRunYet));
 
-        const plotStyle = computed(() => (props.fadePlot ? "opacity:0.5;" : ""));
         const solution = computed(() => (store.state.model.odinSolution));
 
-        // mrc-3331 start time should always be zero.
-        const startTime = 0;
         const endTime = computed(() => store.state.model.endTime);
 
         const palette = computed(() => store.state.model.paletteModel);
 
-        const plot = ref<null | HTMLElement>(null); // Picks up the element with 'plot' ref in the template
-        const baseData = ref<WodinPlotData>([]);
-        const nPoints = 1000; // TODO: appropriate value could be derived from width of element
-
-        const hasPlotData = computed(() => !!(baseData.value?.length));
-
-        const allPlotData = (start: number, end: number): WodinPlotData => {
-            const result = solution.value(start, end, nPoints);
+        const allPlotData = (start: number, end: number, points: number): WodinPlotData => {
+            const result = solution.value(start, end, points);
             if (!result) {
                 return [];
             }
             return [...odinToPlotly(result, palette.value)];
         };
 
-        const config = {
-            responsive: true
-        };
-
-        // This is enough top margin to accomodate the plotly options
-        // bar without it interfering with the first series in the
-        // legend.
-        const margin = {
-            t: 25
-        };
-
-        const relayout = async (event: PlotRelayoutEvent) => {
-            let data;
-            if (event["xaxis.autorange"] === true) {
-                data = baseData.value;
-            } else {
-                const t0 = event["xaxis.range[0]"];
-                const t1 = event["xaxis.range[1]"];
-                if (t0 === undefined || t1 === undefined) {
-                    return;
-                }
-                data = allPlotData(t0, t1);
-            }
-
-            const layout = {
-                margin,
-                uirevision: "true",
-                xaxis: { autorange: true },
-                yaxis: { autorange: true }
-            };
-
-            const el = plot.value as HTMLElement;
-            await react(el, data, layout, config);
-        };
-
-        const resize = () => {
-            if (plot.value) {
-                Plots.resize(plot.value as HTMLElement);
-            }
-        };
-
-        let resizeObserver: null | ResizeObserver = null;
-
-        const drawPlot = () => {
-            if (solution.value) {
-                baseData.value = allPlotData(startTime, endTime.value);
-
-                if (hasPlotData.value) {
-                    const el = plot.value as unknown;
-                    const layout = {
-                        margin
-                    };
-                    newPlot(el as HTMLElement, baseData.value, layout, config);
-                    (el as EventEmitter).on("plotly_relayout", relayout);
-                    resizeObserver = new ResizeObserver(resize);
-                    resizeObserver.observe(plot.value as HTMLElement);
-                }
-            }
-        };
-
-        onMounted(drawPlot);
-
-        watch(solution, drawPlot);
-
-        onUnmounted(() => {
-            if (resizeObserver) {
-                resizeObserver.disconnect();
-            }
-        });
-
         return {
             placeholderMessage,
-            plotStyle,
-            plot,
-            relayout,
-            resize,
+            endTime,
             solution,
-            baseData,
-            hasPlotData
+            allPlotData
         };
     }
 });
 </script>
-<style scoped lang="scss">
-  .plot-placeholder {
-    width: 100%;
-    height: 450px;
-    background-color: #eee;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-  }
-</style>
