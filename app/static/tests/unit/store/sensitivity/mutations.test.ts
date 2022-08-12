135061c1e2b2624328f6f6d7df9971741403e83c
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-08-12 15:52:05 +0100

    mutation tests

--- app/static/tests/unit/store/sensitivity/mutations.test.ts
+++ app/static/tests/unit/store/sensitivity/mutations.test.ts
@@ -13,10 +13,29 @@ describe("Sensitivity mutations", () => {
 
     it("sets param settings", () => {
         const state = {
-            paramSettings: {}
+            paramSettings: {},
+            sensitivityUpdateRequired: false
         } as any;
         const newSettings = { parameterToVary: "A" };
         mutations[SensitivityMutation.SetParamSettings](state, newSettings);
         expect(state.paramSettings).toBe(newSettings);
+        expect(state.sensitivityUpdateRequired).toBe(true);
+    });
+
+    it("sets batch", () => {
+        const state = {
+            batch: null
+        } as any;
+        const batch = {solutions: []} as any
+        mutations[SensitivityMutation.SetBatch](state, batch);
+        expect(state.batch).toBe(batch);
+    });
+
+    it("sets update required", () => {
+        const state = {
+            sensitivityUpdateRequired: false
+        } as any;
+        mutations[SensitivityMutation.SetUpdateRequired](state, true);
+        expect(state.sensitivityUpdateRequired).toBe(true);
     });
 });
