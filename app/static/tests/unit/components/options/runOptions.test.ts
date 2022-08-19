21cb0d4c1e0df0eaf953209e9d120459dc7bc6a3
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-08-19 10:24:05 +0100

    use NumericInputs for end times

--- app/static/tests/unit/components/options/runOptions.test.ts
+++ app/static/tests/unit/components/options/runOptions.test.ts
@@ -2,6 +2,7 @@ import Vuex from "vuex";
 import { shallowMount } from "@vue/test-utils";
 import { BasicState } from "../../../../src/app/store/basic/state";
 import RunOptions from "../../../../src/app/components/options/RunOptions.vue";
+import NumericInput from "../../../../src/app/components/options/NumericInput.vue";
 
 describe("RunOptions", () => {
     const getWrapper = (mockSetEndTime = jest.fn(), mockSetSensitivityUpdateRequired = jest.fn()) => {
@@ -35,18 +36,17 @@ describe("RunOptions", () => {
     it("renders as expected", () => {
         const wrapper = getWrapper();
         expect(wrapper.find("label").text()).toBe("End time");
-        const input = wrapper.find("input");
-        expect(input.attributes("type")).toBe("number");
-        expect(input.attributes("min")).toBe("1");
-        expect((input.element as HTMLInputElement).value).toBe("99");
+        const input = wrapper.findComponent(NumericInput);
+        expect(input.props("value")).toBe(99);
+        expect(input.props("allowNegative")).toBe(false);
     });
 
     it("commits end time change", () => {
         const mockSetEndTime = jest.fn();
         const mockSetSensitivityUpdateRequired = jest.fn();
         const wrapper = getWrapper(mockSetEndTime, mockSetSensitivityUpdateRequired);
-        const input = wrapper.find("input");
-        input.setValue("101");
+        const input = wrapper.findComponent(NumericInput);
+        input.vm.$emit("update", 101);
         expect(mockSetEndTime).toHaveBeenCalledTimes(1);
         expect(mockSetEndTime.mock.calls[0][1]).toBe(101);
         expect(mockSetSensitivityUpdateRequired).toHaveBeenCalledTimes(1);
