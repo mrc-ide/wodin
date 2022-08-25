4f543bd3e76cecbdf1846d7650146c48f9beb27c
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-08-25 11:18:11 +0100

    fix test

--- app/server/tests/integration/session.test.ts
+++ app/server/tests/integration/session.test.ts
@@ -3,35 +3,35 @@ import {
 } from "./utils";
 
 describe("Session integration", () => {
-    afterEach(() => {
-        flushRedis();
+    afterEach(async () => {
+        await flushRedis();
     });
 
     const redisKeyPrefix = "WODIN Example:day1:sessions:";
     const sessionId = "1234";
+    const url = `apps/day1/sessions/${sessionId}`;
 
     it("can post new session", async () => {
         const data = { test: "value" };
-        const response = await post(`apps/day1/sessions/${sessionId}`, data);
+        const response = await post(url, data);
         expect(response.status).toBe(200);
-        await expectRedisJSONValue(`${redisKeyPrefix}:data`, "1234", data);
-        const time = await getRedisValue(`${redisKeyPrefix}:data`, "1234");
+        await expectRedisJSONValue(`${redisKeyPrefix}data`, sessionId, data);
+        const time = await getRedisValue(`${redisKeyPrefix}time`, sessionId);
         const date = Date.parse(time!);
         expect(Date.now() - date).toBeLessThan(1000); // expect saved time to be in last second
     });
 
     it("can update session", async () => {
         const oldData = { test: "oldValue" };
-        const url = "apps/day1/sessions/1234";
         await post(url, oldData);
-        const oldTime = await getRedisValue("WODIN Example:day1:sessions:time", "1234");
+        const oldTime = await getRedisValue(`${redisKeyPrefix}time`, sessionId);
 
         const newData = { test: "newValue" };
         const response = await post(url, newData);
         expect(response.status).toBe(200);
-        await expectRedisJSONValue("WODIN Example:day1:sessions:data", "1234", newData);
+        await expectRedisJSONValue(`${redisKeyPrefix}data`, sessionId, newData);
 
-        const newTime = await getRedisValue("WODIN Example:day1:sessions:time", "1234");
+        const newTime = await getRedisValue(`${redisKeyPrefix}time`, sessionId);
         expect(Date.parse(newTime!)).toBeGreaterThan(Date.parse(oldTime!));
     });
 });
