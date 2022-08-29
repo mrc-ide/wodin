0098e7c1c797b6ec8c74b5884f018c2acd10386b
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-08-29 11:14:17 +0100

    Add test of missing module

--- app/static/tests/unit/components/options/runOptions.test.ts
+++ app/static/tests/unit/components/options/runOptions.test.ts
@@ -7,32 +7,35 @@ import NumericInput from "../../../../src/app/components/options/NumericInput.vu
 
 describe("RunOptions", () => {
     const getWrapper = (mockSetEndTime = jest.fn(), mockSetSensitivityUpdateRequired = jest.fn(),
-        mockDataEnd = 0) => {
-        const store = new Vuex.Store<BasicState>({
-            state: {} as any,
-            modules: {
-                run: {
-                    namespaced: true,
-                    state: {
-                        endTime: 99
-                    },
-                    mutations: {
-                        SetEndTime: mockSetEndTime
-                    }
+        mockDataEnd: number | null = 0) => {
+        const modules = {
+            run: {
+                namespaced: true,
+                state: {
+                    endTime: 99
                 },
-                fitData: {
-                    namespaced: true,
-                    getters: {
-                        [FitDataGetter.dataEnd]: () => mockDataEnd
-                    }
-                },
-                sensitivity: {
-                    namespaced: true,
-                    mutations: {
-                        SetUpdateRequired: mockSetSensitivityUpdateRequired
-                    }
+                mutations: {
+                    SetEndTime: mockSetEndTime
+                }
+            },
+            sensitivity: {
+                namespaced: true,
+                mutations: {
+                    SetUpdateRequired: mockSetSensitivityUpdateRequired
                 }
             }
+        } as any;
+        if (mockDataEnd !== null) {
+            modules.fitData = {
+                namespaced: true,
+                getters: {
+                    [FitDataGetter.dataEnd]: () => mockDataEnd
+                }
+            };
+        }
+        const store = new Vuex.Store<BasicState>({
+            state: {} as any,
+            modules
         });
         return shallowMount(RunOptions, {
             global: {
@@ -55,6 +58,14 @@ describe("RunOptions", () => {
         expect(labels.length).toBe(2);
         expect(labels[0].text()).toBe("End time");
         expect(labels[1].text()).toBe("10 (from data)");
+    });
+
+    it("renders as expected when no fitData module present", () => {
+        const wrapper = getWrapper(jest.fn(), jest.fn(), null);
+        expect(wrapper.find("label").text()).toBe("End time");
+        const input = wrapper.findComponent(NumericInput);
+        expect(input.props("value")).toBe(99);
+        expect(input.props("allowNegative")).toBe(false);
     });
 
     it("commits end time change", () => {
