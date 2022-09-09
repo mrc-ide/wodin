e71f10884a1620e37b992d0fe0fd41d6280f342f
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-09-09 13:57:48 +0100

    Fix path change with most recent node
    Turns out it was not __dirname, but the way that we passed $(dirname $0)
    through which ended up using /tmp (I presume that node has changed how
    it launches scripts between versions). I've checked running the server
    from both the server root and the test directory and the path resolves
    correctly both ways around. I've printed the resolved version to the
    console though to help with debugging.

--- app/server/src/server/args.ts
+++ app/server/src/server/args.ts
@@ -1,11 +1,14 @@
 const yargs = require("yargs/yargs");
+const path = require("path");
 const { hideBin } = require("yargs/helpers");
 
 const { argv } = yargs(hideBin(process.argv));
-const configPath = argv.config;
-if (!configPath) {
+const configPathGiven = argv.config;
+if (!configPathGiven) {
     throw new Error("Please provide a 'config' argument specifying path to the config folder");
 }
-console.log(`Config path: ${configPath}`);
+const configPath = path.resolve(configPathGiven);
+
+console.log(`Config path: ${configPathGiven} (${configPath})`);
 
 module.exports = { configPath };
