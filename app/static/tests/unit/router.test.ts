3a7af5dba04daa9c50b0eb32d12a36480943cf87
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-09-12 16:18:35 +0100

    fix router test

--- app/static/tests/unit/router.test.ts
+++ app/static/tests/unit/router.test.ts
@@ -25,17 +25,8 @@ describe("router", () => {
         jest.clearAllMocks();
     });
 
-    const mockLocation = (href: string) => {
-        const loc = {
-            href
-        } as any;
-        delete (window as any).location;
-        window.location = loc;
-    };
-
-    const expectRouter = (href: string) => {
-        mockLocation(href);
-        const router = getRouter(WodinSession);
+    it("can getRouter", () => {
+        const router = getRouter(WodinSession, "day1");
         expect(router).toBe(mockRouter);
         expect(mockCreateWebHistory).toHaveBeenCalledTimes(1);
         expect(mockCreateWebHistory).toHaveBeenCalledWith("/apps/day1");
@@ -54,13 +45,5 @@ describe("router", () => {
         };
         expect(mockCreateRouter).toHaveBeenCalledTimes(1);
         expect(mockCreateRouter).toHaveBeenCalledWith(expectedRouterOptions);
-    };
-
-    it("can getRouter", () => {
-        expectRouter("http://testwodin.com/apps/day1");
-    });
-
-    it("can getRouter when location is sessions path", () => {
-        expectRouter("http://testwodin.com/apps/day1/sessions");
     });
 });
