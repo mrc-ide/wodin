645b8693256dbd4ad1646da89f984e8bbe2fbaed
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-09-13 10:43:21 +0100

    add comment highlighting router base url

--- app/static/src/app/router.ts
+++ app/static/src/app/router.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory, RouteComponent } from "vue-router";
 import SessionsPage from "./components/sessions/SessionsPage.vue";
 
 export function getRouter(appComponent: RouteComponent, appName: string) {
+    // We use a base route of /app/:appName as each app should be effectively a separate application, with
+    // /sessions relative to the app
     const routeBase = `/apps/${appName}`;
 
     return createRouter({
