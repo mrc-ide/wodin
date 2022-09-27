f03aaa1807f5d2122400ae5bd0c2572cef54a53c
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-09-27 11:19:20 +0100

    basic browser test

--- app/static/tests/e2e/sessions.etest.ts
+++ app/static/tests/e2e/sessions.etest.ts
@@ -2,12 +2,21 @@ import {
     expect, test, chromium, Page
 } from "@playwright/test";
 import * as os from "os";
-import { writeCode } from "./utils";
+import {
+    writeCode,
+    newFitCode,
+    realisticFitData,
+    startModelFit,
+    waitForModelFitCompletion
+} from "./utils";
+import PlaywrightConfig from "../../playwright.config";
 
-const appUrl = "/apps/day1";
+const appUrl = "/apps/day2";
 const saveSessionTimeout = 3000;
 
 test.describe("Sessions tests", () => {
+    const { timeout } = PlaywrightConfig;
+
     test("can navigate to Sessions page from navbar, and load a session", async () => {
         // We need to use a browser with persistent context instead of the default incognito browser so that
         // we can use the session ids in local storage
@@ -16,14 +25,54 @@ test.describe("Sessions tests", () => {
         const page = await browser.newPage();
 
         await page.goto(appUrl);
+
         // change code in this session, which we will later reload and check that we can see the code changes
-        await writeCode(page, "## NEW CODE FOR LOADED SESSION");
+        console.log("2")
+        await page.click(":nth-match(.wodin-left .nav-tabs a, 2)");
+        await writeCode(page, newFitCode);
+        console.log("3")
+        await expect(await page.locator(".run-tab .action-required-msg")).toHaveText(
+            "Model code has been updated. Compile code and Run Model to update.", {
+                timeout
+            }
+        );
+
+        // compile and re-run
+        await page.click("#compile-btn");
+        console.log("3.1")
+        await expect(await page.locator(".run-tab .action-required-msg")).toHaveText(
+            "Plot is out of date: model code has been recompiled. Run model to update.", {
+                timeout
+            }
+        );
+        console.log("3.2")
+        await page.click("#run-btn");
+        console.log("3.3")
+        await expect(await page.locator(".run-tab .action-required-msg")).toHaveText("", { timeout });
+        console.log("3.4")
+
+        // Upload data, link, select variables to vary and run fit
+        await page.click(":nth-match(.wodin-left .nav-tabs a, 1)");
+        console.log("3.5")
+        await startModelFit(page, realisticFitData);
+        console.log("3.6")
+        await waitForModelFitCompletion(page);
+        console.log("3.7")
+
+        // Run sensitivity
+        await page.click(":nth-match(.wodin-right .nav-tabs a, 3)");
+        page.click("#run-sens-btn");
+
+        // TODO: replace this with hidden element check
+        const linesSelector = `.wodin-right .wodin-content .js-plotly-plot .scatterlayer .trace .lines path`;
+        expect((await page.locator(`:nth-match(${linesSelector}, 30)`).getAttribute("d"))!.startsWith("M36")).toBe(true);
+
         // give the page a chance to save the session to back end
         await page.waitForTimeout(saveSessionTimeout);
 
         // Reload the page to get fresh session, and give it time to save new session Id
         await page.goto(appUrl);
-        await page.waitForTimeout(saveSessionTimeout);
+        console.log("4")
 
         // Get storage state in order to test that something has been written to it - but reading it here also seems
         // to force Playwright to wait long enough for storage values to be available in the next page, so the session
@@ -34,6 +83,7 @@ test.describe("Sessions tests", () => {
         await page.click("#sessions-menu");
         await page.click("#all-sessions-link");
 
+        console.log("5")
         await expect(await page.innerText(".container h2")).toBe("Sessions");
         await expect(await page.innerText(":nth-match(.session-col-header, 1)")).toBe("Saved");
         await expect(await page.innerText(":nth-match(.session-col-header, 2)")).toBe("Label");
@@ -41,18 +91,34 @@ test.describe("Sessions tests", () => {
 
         await expect(await page.innerText(".session-label")).toBe("--no label--");
 
-        // NB this will load the second load link, i.e. the older one
-        // If we loaded the most recent (i.e. current session), it would do just do a vue router navigate, not a
-        // reload+rehydrate, so we would not see the placeholder code
+
+        console.log("6")
+
+        // NB this will load the second load link, i.e. the older session, not the current one
         await page.click(":nth-match(.session-load a, 2)");
 
+        console.log("7")
+
+        // Check data
+        await page.waitForTimeout(saveSessionTimeout);
+        await expect(await page.innerText(".wodin-left .nav-tabs .active")).toBe("Data");
+        await expect(await page.innerText("#data-upload-success")).toBe(" Uploaded 32 rows and 2 columns");
+
+        console.log("8")
+
+        // Check code
+        await page.click(":nth-match(.wodin-left .nav-tabs a, 2)");
         await expect(await page.innerText(".wodin-left .nav-tabs .active")).toBe("Code");
         const editorSelector = ".wodin-left .wodin-content .editor-container .editor-scrollable";
         // wait until there is some text in the code editor
         await page.waitForFunction((selector) => !!document.querySelector(selector)?.textContent, editorSelector);
-
         // Check edited code
-        await expect(await page.innerText(editorSelector)).toBe("## NEW CODE FOR LOADED SESSION");
+        await expect(await page.innerText(editorSelector)).toContain("# JUST CHANGE A COMMENT");
+
+        // Check options
+        console.log("9")
+
+        // Check that run, fit and sensitivity have been run
 
         await browser.close();
     });
--- app/static/tests/e2e/utils.ts
+++ app/static/tests/e2e/utils.ts
@@ -1,4 +1,67 @@
-import { Page } from "@playwright/test";
+import {expect, Page} from "@playwright/test";
+
+export const newFitCode = `# JUST CHANGE A COMMENT
+initial(S) <- N - I_0
+initial(E) <- 0
+initial(I) <- I_0
+initial(R) <- 0
+
+# equations
+deriv(S) <- -beta * S * I / N
+deriv(E) <- beta * S * I / N - gamma * E
+deriv(I) <- gamma * E - sigma * I
+deriv(R) <- sigma * I
+
+# parameter values
+R_0 <- user(1.5)
+L <- user(1)
+D <- user(1)
+I_0 <- 1 # default value
+N <- 370
+
+# convert parameters
+gamma <- 1 / L
+sigma <- 1 / D
+beta <- R_0 * sigma
+
+#Output
+output(onset) <- if(t == 0) I_0 else gamma*E
+`;
+
+export const realisticFitData = `Day,Cases
+0,1
+1,1
+2,0
+3,2
+4,5
+5,3
+6,3
+7,3
+8,6
+9,2
+10,5
+11,9
+12,13
+13,12
+14,13
+15,11
+16,12
+17,6
+18,6
+19,6
+20,3
+21,1
+22,0
+23,2
+24,0
+25,0
+26,0
+27,0
+28,2
+29,0
+30,2
+31,0
+`;
 
 export const uploadCSVData = async (page: Page, data: string) => {
     const file = {
@@ -14,4 +77,37 @@ export const writeCode = async (page: Page, code: string) => {
     await page.press(".monaco-editor textarea", "Delete");
     await page.fill(".monaco-editor textarea", "");
     await page.fill(".monaco-editor textarea", code);
+};
+
+export const startModelFit = async (page: Page, data: string = realisticFitData) => {
+    // Upload data
+    await uploadCSVData(page, data);
+    await page.click(":nth-match(.wodin-right .nav-tabs a, 2)");
+
+    // link variables
+    await page.click(":nth-match(.wodin-left .nav-tabs a, 3)");
+    await expect(await page.innerText("#optimisation")).toBe(
+        "Please link at least one column in order to set target to fit."
+    );
+    const linkContainer = await page.locator(":nth-match(.collapse .container, 1)");
+    const select1 = await linkContainer.locator(":nth-match(select, 1)");
+    await select1.selectOption("I");
+
+    await expect(await page.innerText("#optimisation label#target-fit-label")).toBe("Cases ~ I");
+
+    // select param to vary
+    await page.click(":nth-match(.wodin-right .nav-tabs a, 2)");
+    await expect(await page.innerText(".wodin-right .wodin-content .nav-tabs .active")).toBe("Fit");
+    await expect(await page.innerText("#select-param-msg"))
+        .toBe("Please select at least one parameter to vary during model fit.");
+    await page.click(":nth-match(input.vary-param-check, 1)");
+    await expect(await page.innerText("#select-param-msg")).toBe("");
+
+    // run fit
+    await expect(await page.innerText(".wodin-right .wodin-content .nav-tabs .active")).toBe("Fit");
+    await page.click(".wodin-right .wodin-content div.mt-4 button#fit-btn");
+};
+
+export const waitForModelFitCompletion = async (page: Page) => {
+    await expect(await page.getAttribute(".wodin-plot-container .vue-feather", "data-type")).toBe("check");
 };
