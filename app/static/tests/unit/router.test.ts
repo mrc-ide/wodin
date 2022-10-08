601f8091eff2eba2d0e5113cff7a3d00600b1a90
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-10-08 15:09:21 +0100

    add test

--- app/static/tests/unit/router.test.ts
+++ app/static/tests/unit/router.test.ts
@@ -20,12 +20,34 @@ mockCreateWebHistory.mockReturnValue(mockWebHistory);
 const mockCreateRouter = createRouter as Mock;
 mockCreateRouter.mockReturnValue(mockRouter);
 
+const mockPushState = jest.fn();
+
+const realLocation = window.location;
+const realHistory = window.history;
+
 describe("router", () => {
     beforeEach(() => {
         jest.clearAllMocks();
     });
 
-    it("can getRouter", () => {
+    beforeAll(() => {
+        delete (window as any).location;
+        window.location = {
+            href: "http://localhost:3000/apps/day1"
+        } as any;
+
+        delete (window as any).history;
+        window.history = {
+            pushState: mockPushState
+        } as any;
+    });
+
+    afterAll(() => {
+        window.location = realLocation;
+        window.history = realHistory;
+    });
+
+    it("can initialise router", () => {
         const router = initialiseRouter(WodinSession, "day1");
         expect(router).toBe(mockRouter);
         expect(mockCreateWebHistory).toHaveBeenCalledTimes(1);
@@ -45,5 +67,18 @@ describe("router", () => {
         };
         expect(mockCreateRouter).toHaveBeenCalledTimes(1);
         expect(mockCreateRouter).toHaveBeenCalledWith(expectedRouterOptions);
+
+        expect(mockPushState).not.toHaveBeenCalled();
+    });
+
+    it("initialiseRouter removes session id from current url", () => {
+        window.location.href = "http://localhost:3000/apps/day1/?sessionId=9876";
+        const router = initialiseRouter(WodinSession, "day1");
+        expect(router).toBe(mockRouter);
+
+        expect(mockPushState).toHaveBeenCalledTimes(1);
+        expect(mockPushState.mock.calls[0][0]).toBe(null);
+        expect(mockPushState.mock.calls[0][1]).toBe("");
+        expect(mockPushState.mock.calls[0][2]).toBe("http://localhost:3000/apps/day1/");
     });
 });
