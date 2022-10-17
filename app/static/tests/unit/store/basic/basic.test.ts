ecb75788d98ba1c3fd4fa15b0fde3be2961261a5
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-10-17 21:43:13 +0100

    test fix

--- app/static/tests/unit/store/basic/basic.test.ts
+++ app/static/tests/unit/store/basic/basic.test.ts
@@ -5,13 +5,11 @@ jest.mock("../../../../src/app/utils", () => {
         newSessionId: jest.fn().mockReturnValue("12345")
     };
 });
-const spyAddSessionId = jest.spyOn(localStorageManager, "addSessionId");
 
 describe("basic", () => {
     it("generates and saves sessionId", async () => {
         const { storeOptions } = await import("../../../../src/app/store/basic/basic");
         const state = storeOptions.state as any;
         expect(state.sessionId).toBe("12345");
-        expect(spyAddSessionId).toHaveBeenCalledWith("12345");
     });
 });
--- app/static/tests/unit/store/fit/fit.test.ts
+++ app/static/tests/unit/store/fit/fit.test.ts
@@ -5,13 +5,11 @@ jest.mock("../../../../src/app/utils", () => {
         newSessionId: jest.fn().mockReturnValue("12345")
     };
 });
-const spyAddSessionId = jest.spyOn(localStorageManager, "addSessionId");
 
 describe("fit", () => {
     it("generates and saves sessionId", async () => {
         const { storeOptions } = await import("../../../../src/app/store/fit/fit");
         const state = storeOptions.state as any;
         expect(state.sessionId).toBe("12345");
-        expect(spyAddSessionId).toHaveBeenCalledWith("12345");
     });
 });
