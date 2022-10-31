30a885c330e814b6e60e326950d86072b84ac332
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-10-31 15:59:43 +0000

    Unpin branch after merge

--- scripts/run-dependencies.sh
+++ scripts/run-dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-ODIN_API_BRANCH=odin-api-mrc-3745
+ODIN_API_BRANCH=main
 
 docker pull mrcide/odin.api:$ODIN_API_BRANCH
 docker run -d --name odin.api --rm -p 8001:8001 mrcide/odin.api:$ODIN_API_BRANCH
