d3dd26d1469760e85f2f3835331f56d0498eae54
M-Kusumgar <mantrakusumgar@outlook.com>
2023-02-17 11:14:07 +0000

    add config files for fit and stochastic apps with commands

---
+++ app/static/hotReloadConfigs/fitApp.config.js
@@ -0,0 +1,31 @@
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+const path = require("path")
+
+module.exports = {
+    lintOnSave: false,
+    outputDir: path.join(__dirname, "../../server/public/js"),
+    runtimeCompiler: true,
+    configureWebpack: {
+        plugins: [new MonacoWebpackPlugin({
+            languages: ["r"]
+        })],
+        devServer: {
+            writeToDisk: true
+        },
+        output: {
+            filename: "fitApp.umd.min.js",
+            libraryTarget: "umd",
+            library: "lib",
+            hotUpdateChunkFilename: "../hot/hot-update.js",
+            hotUpdateMainFilename: "../hot/hot-update.json"
+        },
+        optimization: {
+            splitChunks: false
+        }
+    },
+    css: {
+        extract: {
+            filename: "../css/fitApp.css"
+        }
+    }
+};
