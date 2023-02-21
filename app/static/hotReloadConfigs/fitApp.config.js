2d86f6a67fcfe1975cec46ff628a9cb92654a949
Mantra <mantrakusumgar@outlook.com>
2023-02-21 12:24:34 +0000

    abstract out code from hot reload configs into getAppConfig

--- app/static/hotReloadConfigs/fitApp.config.js
+++ app/static/hotReloadConfigs/fitApp.config.js
@@ -1,31 +1,3 @@
-const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
-const path = require("path")
+const getAppConfig = require("./getAppConfig");
 
-module.exports = {
-    lintOnSave: false,
-    outputDir: path.join(__dirname, "../../server/public/js"),
-    runtimeCompiler: true,
-    configureWebpack: {
-        plugins: [new MonacoWebpackPlugin({
-            languages: ["r"]
-        })],
-        devServer: {
-            writeToDisk: true
-        },
-        output: {
-            filename: "fitApp.umd.min.js",
-            libraryTarget: "umd",
-            library: "lib",
-            hotUpdateChunkFilename: "../hot/hot-update.js",
-            hotUpdateMainFilename: "../hot/hot-update.json"
-        },
-        optimization: {
-            splitChunks: false
-        }
-    },
-    css: {
-        extract: {
-            filename: "../css/fitApp.css"
-        }
-    }
-};
+module.exports = getAppConfig("fit");
