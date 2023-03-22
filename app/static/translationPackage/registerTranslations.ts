815c5e4b91283899176c84016bab88c86be49904
Mantra <mantrakusumgar@outlook.com>
2023-03-22 10:32:48 +0000

    add function to check if keys are matching and throw error if there are duplicate keys for the same language

--- app/static/translationPackage/registerTranslations.ts
+++ app/static/translationPackage/registerTranslations.ts
@@ -1,15 +1,52 @@
-import { Store } from "vuex";
 import i18next from "i18next";
-import { codeTabLocales } from "../src/app/store/translations/codeTabLocales";
-import { AppState } from "../src/app/store/appState/state";
 import { LanguageState } from "./store/state";
 
-export default (langState: LanguageState) => {
+interface Resource {
+    [key: string]: string
+}
+
+type ResourceObject<L extends string> = {
+    [key in L]: Resource[]
+}
+
+export default function registerTranslations<L extends string>(languageState: LanguageState, resourceObject: ResourceObject<L>) {
     i18next.init({
-        lng: langState.currentLanguage,
+        lng: languageState.currentLanguage,
         fallbackLng: "en",
-        resources: {}
+        resources: {
+            en: { translation: { hey: "hello"} }
+        }
     });
-    i18next.addResources("fr", "translation", codeTabLocales.fr)
-    i18next.addResources("en", "translation", codeTabLocales.en)
+    const languages = Object.keys(resourceObject) as L[];
+    languages.forEach(lng => {
+        registerResources(lng, resourceObject[lng]);
+    })
+};
+
+const registerResources = (lng: string, resources: Resource[]) => {
+    resources.forEach(resource => {
+        addResource(lng, resource);
+    })
+};
+
+const addResource = (lng: string, resource: Resource) => {
+    console.log(lng);
+    console.log(resource);
+
+    const existingBundle = i18next.getResourceBundle(lng, "translation");
+    if (!existingBundle) {
+        i18next.addResources(lng, "translation", resource);
+        return
+    }
+    const existingKeys = Object.keys(existingBundle);
+    const resourceKeys = Object.keys(resource);
+    const commonKeys = existingKeys.filter(key => {
+        return resourceKeys.indexOf(key) > -1;
+    });
+    if (commonKeys.length > 0) {
+        throw new Error(`The keys [${commonKeys.join(", ")}] are shared by more than
+        one resource bundle. `)
+    } else {
+        i18next.addResources(lng, "translation", resource);
+    }
 };
