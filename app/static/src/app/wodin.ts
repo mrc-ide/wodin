281325a1ef50242bea6d1eff66973e4ae5052357
Mantra <mantrakusumgar@outlook.com>
2023-03-22 11:02:47 +0000

    add locales to wodin

--- app/static/src/app/wodin.ts
+++ app/static/src/app/wodin.ts
@@ -15,6 +15,17 @@ import { initialiseRouter } from "./router";
 import tooltip from "./directives/tooltip";
 import { AppType } from "./store/appState/state";
 import registerTranslations from "../../translationPackage/registerTranslations"
+import { codeTabLocales } from "./store/translations/codeTab";
+import { dataTabLocales } from "./store/translations/dataTab";
+import { fitTabLocales } from "./store/translations/fitTab";
+import { genericHelpLocales } from "./store/translations/genericHelp";
+import { headerLocales } from "./store/translations/header";
+import { indexPageLocales } from "./store/translations/indexPage";
+import { optionsTabLocales } from "./store/translations/optionsTab";
+import { runTabLocales } from "./store/translations/runTab";
+import { sensitivityTabLocales } from "./store/translations/sensitivityTab";
+import { stochasticHelpLocales } from "./store/translations/stochasticHelpTab";
+import { sharedLocales } from "./store/translations/shared";
 
 declare let appType: AppType;
 
@@ -46,7 +57,35 @@ const getComponent = () => {
 };
 
 export const store = getStore();
-registerTranslations(store.state.language);
+
+registerTranslations(store.state.language, {
+    en: [
+        codeTabLocales.en,
+        dataTabLocales.en,
+        fitTabLocales.en,
+        genericHelpLocales.en,
+        headerLocales.en,
+        indexPageLocales.en,
+        optionsTabLocales.en,
+        runTabLocales.en,
+        sensitivityTabLocales.en,
+        stochasticHelpLocales.en,
+        sharedLocales.en
+    ],
+    fr: [
+        codeTabLocales.fr,
+        dataTabLocales.fr,
+        fitTabLocales.fr,
+        genericHelpLocales.fr,
+        headerLocales.fr,
+        indexPageLocales.fr,
+        optionsTabLocales.fr,
+        runTabLocales.fr,
+        sensitivityTabLocales.fr,
+        stochasticHelpLocales.fr,
+        sharedLocales.fr
+    ]
+});
 
 const app = createApp({ components: { WodinSession, AppHeader } });
 app.use(store);
