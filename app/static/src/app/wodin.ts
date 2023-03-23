d8a04164a0f0a3274bee8e07032107ae93f4b1d5
Mantra <mantrakusumgar@outlook.com>
2023-03-23 10:15:56 +0000

    remove register translations from wodin.ts

--- app/static/src/app/wodin.ts
+++ app/static/src/app/wodin.ts
@@ -14,18 +14,6 @@ import { storeOptions as stochasticStoreOptions } from "./store/stochastic/stoch
 import { initialiseRouter } from "./router";
 import tooltip from "./directives/tooltip";
 import { AppType } from "./store/appState/state";
-import registerTranslations from "../../translationPackage/registerTranslations"
-import { codeTabLocales } from "./store/translations/codeTab";
-import { dataTabLocales } from "./store/translations/dataTab";
-import { fitTabLocales } from "./store/translations/fitTab";
-import { genericHelpLocales } from "./store/translations/genericHelp";
-import { headerLocales } from "./store/translations/header";
-import { indexPageLocales } from "./store/translations/indexPage";
-import { optionsTabLocales } from "./store/translations/optionsTab";
-import { runTabLocales } from "./store/translations/runTab";
-import { sensitivityTabLocales } from "./store/translations/sensitivityTab";
-import { stochasticHelpLocales } from "./store/translations/stochasticHelpTab";
-import { sharedLocales } from "./store/translations/shared";
 
 declare let appType: AppType;
 
@@ -57,35 +45,6 @@ const getComponent = () => {
 };
 
 export const store = getStore();
-
-registerTranslations(store.state.language, {
-    en: [
-        codeTabLocales.en,
-        dataTabLocales.en,
-        fitTabLocales.en,
-        genericHelpLocales.en,
-        headerLocales.en,
-        indexPageLocales.en,
-        optionsTabLocales.en,
-        runTabLocales.en,
-        sensitivityTabLocales.en,
-        stochasticHelpLocales.en,
-        sharedLocales.en
-    ],
-    fr: [
-        codeTabLocales.fr,
-        dataTabLocales.fr,
-        fitTabLocales.fr,
-        genericHelpLocales.fr,
-        headerLocales.fr,
-        indexPageLocales.fr,
-        optionsTabLocales.fr,
-        runTabLocales.fr,
-        sensitivityTabLocales.fr,
-        stochasticHelpLocales.fr,
-        sharedLocales.fr
-    ]
-});
 
 const app = createApp({ components: { WodinSession, AppHeader } });
 app.use(store);
