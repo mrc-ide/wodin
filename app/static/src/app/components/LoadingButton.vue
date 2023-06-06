adcc4c8d7afcfcbf36aadabdffdbc1cf6ce0be2f
Mantra <mantrakusumgar@outlook.com>
2023-06-06 14:42:22 +0100

    add loading button component

---
+++ app/static/src/app/components/LoadingButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <button :disabled="loading || isDisabled">
+        <span v-if="loading" class="spinner-border spinner-border-sm me-2" role="status" aria-hidden="true"></span>
+        <slot></slot>
+    </button>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    props: {
+        loading: Boolean,
+        isDisabled: Boolean
+    }
+})
+</script>
