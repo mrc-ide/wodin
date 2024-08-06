349e141a057a986fff32f366195d7cd81d69989f
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2024-08-06 09:18:28 +0100

    start e2e test

--- app/static/tests/e2e/run.etest.ts
+++ app/static/tests/e2e/run.etest.ts
@@ -14,6 +14,15 @@ const expectXTicks = async (page: Page, expectedGraphCount: number, expectedXTic
     }
 };
 
+const selectGraphRange = async (page: Page) => {
+    // Drag out fixed are on graph to select subrange of both axes
+    const graphBounds = await page.locator(":nth-match(.plot .draglayer .xy .nsewdrag, 1)").boundingBox()!;
+    await page.mouse.move(graphBounds.x + 100, graphBounds.y + 50);
+    await page.mouse.down();
+    await page.mouse.move(graphBounds.x + 300, graphBounds.y + 100);
+    await page.mouse.up();
+};
+
 test.describe("Run Tab", () => {
     test("x axis zoom on plot is replicated in other plots", async ({ page }) => {
         await page.goto("/apps/day1");
@@ -28,14 +37,28 @@ test.describe("Run Tab", () => {
         await expectXTicks(page, 3, [0, 20, 40, 60, 80, 100]);
 
         // 2. Drag to zoom to an area on the first graph
-        const graphBounds = await page.locator(":nth-match(.plot .draglayer .xy .nsewdrag, 1)").boundingBox()!;
-        await page.mouse.move(graphBounds.x + 100, graphBounds.y + 50);
-        await page.mouse.down();
-        await page.mouse.move(graphBounds.x + 300, graphBounds.y + 100);
-        await page.mouse.up();
+        await selectGraphRange(page);
 
         // 3. Check - using x axis ticks - that the expected x axis values are shown on all three graphs.
         await expectXTicks(page, 3, [15, 20, 25, 30, 35, 40]);
+
+        await page.screenshot({path: "screenshot.png"})
+    });
+
+    test("Reset axes button reset both axes", async () => {
+        const initialXTicks = [10, 20, 40, 60, 80, 100];
+        const initialYTicks = ["0", "0.2M", "0.4M", "0.6M", "0.8M", "1M"];
+        // Sanity check initial x and y axes
+
+        // select graph range
+
+        // Check expected subrange axes
+        const subrangeXTicks = [15, 20, 25, 30, 35, 40];
+        const subrangeYTicks = ["600k", "620k", "640k", "660k", "680k", "700k"];
+
+        // Click Reset axes button
+
+        // Check initial x and y axes have been restores
     });
 
     test("x axis Time label is shown for final plot only, in Basic app", async ({ page }) => {
