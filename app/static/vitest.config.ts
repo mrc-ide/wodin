0af98b25e166c28bcfc7b8aa075f488930631a21
Mantra <mantrakusumgar@outlook.com>
2024-11-06 14:24:37 +0000

    wire up codecov

--- app/static/vitest.config.ts
+++ app/static/vitest.config.ts
@@ -10,7 +10,10 @@ export default mergeConfig(
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
       globals: true,
-      silent: true
+      silent: true,
+      coverage: {
+        provider: "istanbul"
+      }
     }
   })
 )
