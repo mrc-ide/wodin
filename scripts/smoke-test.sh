b1784013088067186a9afe7e7e956d383da0a7ff
Mantra <mantrakusumgar@outlook.com>
2024-11-07 15:38:34 +0000

    proper smoke test

---
+++ scripts/smoke-test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+retries=10
+sleep 5;
+
+while ((retries > 0)); do
+    curl http://localhost:3000 | grep "Example WODIN configuration" && break
+
+    echo "retrying connection with server in 1 seconds"
+    sleep 1
+    ((retries --))
+done
+
+if ((retries == 0 )); then
+    echo "Didn't get expected response from the server"
+    exit 1
+fi
