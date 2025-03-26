217b708f9eea1cbfb5a9d8d5d384679ebb5d3200
Mantra <mantrakusumgar@outlook.com>
2025-03-26 17:22:49 +0000

    more terse

--- app/static/src/componentsStatic/ParameterControl.vue
+++ app/static/src/componentsStatic/ParameterControl.vue
@@ -5,7 +5,10 @@
   <div :id="`${name}-description`">
     {{ description }}
   </div>
-  <input type="range" class="form-range" :min="min" :max="max" :step="step || (max - min) / 10000" :value="value" @input="handleChange"/>
+  <input type="range" class="form-range"
+         :min="min" :max="max"
+         :step="step || (max - min) / 100000"
+         :value="value" @input="handleChange"/>
 </template>
 
 <script setup lang="ts">
@@ -27,9 +30,8 @@ const props = defineProps({
 const value = computed(() => store.state.run.parameterValues![props.name]);
 
 const handleChange = (e: Event) => {
-  const newVal = (e.target as HTMLInputElement).value;
-  const oldParameterValues = store.state.run.parameterValues;
-  const newParameterValues = { ...oldParameterValues, [props.name]: parseFloat(newVal) };
-  store.commit(`run/${RunMutation.SetParameterValues}`, newParameterValues);
+  const newVal = parseFloat((e.target as HTMLInputElement).value);
+  const oldParsValue = store.state.run.parameterValues;
+  store.commit(`run/${RunMutation.SetParameterValues}`, { ...oldParsValue, [props.name]: newVal });
 };
 </script>
