22c8c060f65f1bec952cc80bae2d05e7432bff6a
Mantra <mantrakusumgar@outlook.com>
2025-03-31 17:12:35 +0100

    pull mrc-6352 parameter slider changes

--- app/static/src/componentsStatic/ParameterSlider.vue
+++ app/static/src/componentsStatic/ParameterSlider.vue
@@ -1,18 +1,14 @@
 <template>
-  <div>
-    <label :id="`${name}-input`" class="col-form-label">{{ name }}</label>
-  </div>
+  <label :id="`${name}-input`" class="col-form-label fw-bold pb-0">{{ name }}</label>
   <div :id="`${name}-description`">
     {{ description }}
   </div>
-  <input type="range" class="form-range"
-         :min="min" :max="max"
-         :step="step || (max - min) / 100000"
-         :value="value" @input="handleChange"/>
+  <input type="range" class="form-range" :min="min" :max="max" :step="step || (max - min) / 10000" :value="value" @input="handleChange"/>
 </template>
 
 <script setup lang="ts">
 import { AppState } from '@/store/appState/state';
+import { RunAction } from '@/store/run/actions';
 import { RunMutation } from '@/store/run/mutations';
 import { computed } from 'vue';
 import { useStore } from 'vuex';
@@ -30,8 +26,10 @@ const props = defineProps({
 const value = computed(() => store.state.run.parameterValues![props.name]);
 
 const handleChange = (e: Event) => {
-  const newVal = parseFloat((e.target as HTMLInputElement).value);
-  const oldParsValue = store.state.run.parameterValues;
-  store.commit(`run/${RunMutation.SetParameterValues}`, { ...oldParsValue, [props.name]: newVal });
+  const newVal = (e.target as HTMLInputElement).value;
+  const oldParameterValues = store.state.run.parameterValues;
+  const newParameterValues = { ...oldParameterValues, [props.name]: parseFloat(newVal) };
+  store.commit(`run/${RunMutation.SetParameterValues}`, newParameterValues);
+  store.dispatch(`run/${RunAction.RunModel}`);
 };
 </script>
