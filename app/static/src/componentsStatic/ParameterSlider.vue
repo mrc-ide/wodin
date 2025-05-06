7f9bf4ac77df61e6b8641ad095cf64d34264ba30
Mantra <mantrakusumgar@outlook.com>
2025-05-06 16:01:32 +0100

    add title + latex support int0 parameter slider component and updated example

--- app/static/src/componentsStatic/ParameterSlider.vue
+++ app/static/src/componentsStatic/ParameterSlider.vue
@@ -1,40 +1,65 @@
 <template>
-  <label :id="`${name}-input`" class="col-form-label fw-bold pb-0 label">{{ name }}</label>
-  <div :id="`${name}-description`" class="description">
-    {{ description }}
+  <div class="slider">
+    <div style="width: 100%;">
+      <label class="col-form-label fw-bold pb-0 label" ref="labelRef"></label>
+      <div class="desc" ref="descRef"></div>
+    </div>
+    <div style="width: 100%;">
+      <div class="float-end value mt-2">
+        {{ value }}
+      </div>
+      <input type="range"
+             class="form-range"
+             :min="min"
+             :max="max"
+             :step="step || (max - min) / 10000"
+             :value="value"
+             @input="handleChange"/>
+    </div>
   </div>
-  <div :id="`${name}-value`" class="float-end value mt-2">
-    {{ value }}
-  </div>
-  <input type="range" class="form-range" :min="min" :max="max" :step="step || (max - min) / 10000" :value="value" @input="handleChange"/>
 </template>
 
 <script setup lang="ts">
 import { AppState } from '@/store/appState/state';
 import { RunAction } from '@/store/run/actions';
 import { RunMutation } from '@/store/run/mutations';
-import { computed } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { useStore } from 'vuex';
 
 const store = useStore<AppState>();
 
 const props = defineProps({
-  name: { type: String, required: true },
-  description: { type: String, required: false },
+  par: { type: String, required: true },
+  title: { type: String },
+  desc: { type: String },
   min: { type: Number, required: true },
   max: { type: Number, required: true },
   step: { type: Number }
 });
 
-const value = computed(() => store.state.run.parameterValues![props.name]);
+const value = computed(() => store.state.run.parameterValues![props.par]);
 
 const handleChange = (e: Event) => {
   const newVal = (e.target as HTMLInputElement).value;
   const oldParameterValues = store.state.run.parameterValues;
-  const newParameterValues = { ...oldParameterValues, [props.name]: parseFloat(newVal) };
+  const newParameterValues = { ...oldParameterValues, [props.par]: parseFloat(newVal) };
   store.commit(`run/${RunMutation.SetParameterValues}`, newParameterValues);
   store.dispatch(`run/${RunAction.RunModel}`);
 };
+
+const labelRef = ref<HTMLLabelElement | null>(null);
+const descRef = ref<HTMLDivElement | null>(null);
+
+onMounted(async () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const { MathJax } = window as any;
+  if (!labelRef.value || !descRef.value || !MathJax) return;
+
+  labelRef.value.innerHTML = props.title || props.par;
+  descRef.value.innerHTML = props.desc || "";
+
+  await MathJax.typesetPromise();
+});
 </script>
 
 <style lang="css" scoped>
@@ -43,12 +68,19 @@ const handleChange = (e: Event) => {
   color: grey;
 }
 
-.description {
+.desc {
   font-size: 0.85rem;
   color: rgb(82, 82, 82);
 }
 
 .label {
   font-size: 0.9rem;
+}
+
+.slider {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
 </style>
