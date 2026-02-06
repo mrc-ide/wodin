cdf98e310a174585ce265d1f0a297e6eb26f5988
Mantra <mantrakusumgar@outlook.com>
2026-02-06 15:05:18 +0000

    add comments about utility types

--- app/static/tests/testUtils.ts
+++ app/static/tests/testUtils.ts
@@ -30,18 +30,42 @@ export const expectLeftWodinTabs = (wrapper: VueWrapper<any>, expectedTabNames: 
     expectWodinTabs(wrapper, expectedTabNames, "left");
 };
 
+// utility type that removes generic type nesting in typescript LSP
+// preview and shows the type as an actual object
+// see https://gist.github.com/palashmon/db68706d4f26d2dbf187e76409905399
 export type Prettify<T> = {
     [K in keyof T]: T[K];
 } & {};
 
+// makes the field optional if it can be undefined and removes
+// fields that can't be undefined (notice ? after foo)
+//
+// { foo: string | undefined, bar: number } -> { foo?: string | undefined }
 type OptionalKeys<T> = {
     [K in keyof T as undefined extends T[K] ? K : never]?: T[K]
 }
 
+// extracts the fields that cannot undefined
+//
+// { foo: string | undefined, bar: number } -> { bar: number }
 type RequiredKeys<T> = {
     [K in keyof T as undefined extends T[K] ? never : K]: T[K]
 }
 
+// the OptionalKeys and RequiredKeys are needed because vue now types
+// the props defined as
+// {
+//     foo: { type: String, required: true },
+//     bar: { type: String, required: false },
+// }
+//
+// as
+//
+// { foo: string, bar: string | undefined }
+//
+// but it should be
+//
+// { foo: string, bar?: string | undefined }
 export type ComponentProps<T> =
     T extends { setup?: (props: infer Props, ...otherArgs: any[]) => any }
         ? Prettify<RequiredKeys<Props> & OptionalKeys<Props>>
