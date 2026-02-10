3c0538dcf71f8220c5bd0446309fdab585159c0f
Mantra <mantrakusumgar@outlook.com>
2026-02-10 12:48:41 +0000

    more resilient copy script

--- scripts/copy-static-files.sh
+++ scripts/copy-static-files.sh
@@ -1,3 +1,3 @@
 ROOT=$(realpath $(dirname $0)/..)
 PUBLIC_FOLDER=$ROOT/app/server/public
-cp $1/index.html $PUBLIC_FOLDER && cp -r $1/files $PUBLIC_FOLDER && cp -r $1/help $PUBLIC_FOLDER
+cp $1/index.html $PUBLIC_FOLDER && mkdir -p $1/files && cp -r $1/files $PUBLIC_FOLDER && mkdir -p $1/help && cp -r $1/help $PUBLIC_FOLDER
